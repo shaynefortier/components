67cc65c02129897d90b2a19a24a14089aff22f1e
shaynefortier <70u3nulj@larknight.anonaddy.com>
2025-01-04 22:02:22 -0500

    New component: Text Reveal

---
+++ js/TextReveal.js
@@ -0,0 +1,71 @@
+class TextReveal {
+	constructor(elements, options = {}) {
+		this.elements = elements;
+		const defaultOptions = {};
+		this.options = { ...defaultOptions, ...options };
+		this.observer = new IntersectionObserver(this.activate.bind(this), {
+			rootMargin: '0% 0% 0% 0%',
+			threshold: 1,
+		});
+
+		this.init();
+	}
+
+	init() {
+		for (let i = 0; i < this.elements.length; i++) {
+			const element = this.elements[i];
+			// this.observer.observe(element);
+			this.addSplitting(element);
+		}
+		TextReveal.splitWords();
+	}
+
+	activate(entries) {
+		console.log(entries);
+
+		for (let i = 0; i < entries.length; i++) {
+			const entry = entries[i];
+			if (entry.isIntersecting) {
+				const element = entry.target;
+				element.classList.add('active');
+				this.observer.unobserve(element);
+			}
+		}
+	}
+
+	addSplitting(element) {
+		const paragraphs = element.querySelectorAll('p');
+
+		for (let i = 0; i < paragraphs.length; i++) {
+			const paragraph = paragraphs[i];
+			paragraph.setAttribute('data-splitting', 'test');
+			this.observer.observe(paragraph);
+		}
+	}
+
+	static splitWords() {
+		const paragraphs = Splitting({ by: 'lines' });
+
+		for (let i = 0; i < paragraphs.length; i++) {
+			const paragraph = paragraphs[i];
+			let lines = '';
+
+			for (let i = 0; i < paragraph.lines.length; i++) {
+				const line = paragraph.lines[i];
+
+				for (let i = 0; i < line.length; i++) {
+					const word = line[i];
+					word.style.setProperty('--word-index', i);
+				}
+
+				lines += `
+					<span class="line"><div class="words">${line
+						.map((word) => `${word.outerHTML}`)
+						.join('<span class="whitespace"> </span>')}</div></span>
+					`;
+			}
+
+			paragraph.el.innerHTML = lines;
+		}
+	}
+}
